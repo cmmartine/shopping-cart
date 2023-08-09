e7397e820f5d7dc36849ccf0f6ee75bf0fd1cf27
cmmartine <cmmartine@gmail.com>
2023-08-09 15:03:13 -0400

    Add pages, components, browser router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  } ,
+  {
+    path: 'shop',
+    element: <Shop />,
+  },
+  {
+    path: 'cart',
+    element: <Cart />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 );
---
+++ src/pages/Cart.js
@@ -0,0 +1,9 @@
+function Cart() {
+  return(
+    <div>
+      Your Cart
+    </div>
+  );
+}
+
+export default Cart;
