6d3f4e486a1109cde6d5286dc62474a114b5c9f8
cmmartine <cmmartine@gmail.com>
2023-08-10 08:15:50 -0400

    Add navbar structuring and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import '../styles/navbar.css';
 
 function Navbar() {
   return (
-    <div>
-      <Link to='/'>Home</Link>
-      <Link to='/shop'>Shop</Link>
-      <Link to='/cart'>Cart</Link>
+    <div className='navbar'>
+      <div className='nav-left'>
+        <Link to='/' className='nav-link'>Home</Link>
+      </div>
+      <div className='nav-right'>
+        <Link to='/shop' className='nav-link'>Shop</Link>
+        <Link to='/cart' className='nav-link'>Cart</Link>
+      </div>
     </div>
   );
 }
