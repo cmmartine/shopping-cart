a5db0a033ad02cb63f867b7eaaaa1a5f5c9c7f2f
cmmartine <cmmartine@gmail.com>
2023-08-15 14:31:41 -0400

    Add tests for CartProductCard

--- src/tests/CartProductCard.test.js
+++ src/tests/CartProductCard.test.js
@@ -1,12 +1,13 @@
 import renderer from 'react-test-renderer';
 import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import CartProductCard from '../app/components/CartProductCard';
 import { renderWithProviders } from './test_utils/redux-store-test-util';
 import { store } from '../app/store';
 import { Provider } from 'react-redux';
 
 describe('Cart Product Card', () => {
-  const prop = {
+  const product = {
     prodName: 'Shoes',
     prodImg: 'Some image',
     prodDesc: 'Very good',
@@ -15,12 +16,21 @@ describe('Cart Product Card', () => {
     id: 1,
   }
 
+  const productInCart = {
+    prodName: 'Shoes',
+    prodImg: 'Some image',
+    prodDesc: 'Very good',
+    prodCost: '$49.99',
+    numInCart: 3,
+    id: 1,
+  }
+
   it('renders correctly', () => {
 
     const view = renderer
       .create(
         <Provider store={store}>
-        <CartProductCard product={prop}/>
+        <CartProductCard product={product}/>
       </Provider>
       )
       .toJSON();
@@ -30,9 +40,23 @@ describe('Cart Product Card', () => {
 
   it('renders the card passed to it', () => {
 
-    renderWithProviders(<CartProductCard product={prop} />);
+    renderWithProviders(<CartProductCard product={product} />);
 
     expect(screen.getByRole('heading').textContent).toMatch('Shoes');
+  });
+
+  it('renders correct quantity when numInCart is 0', () => {
+
+    renderWithProviders(<CartProductCard product={product} />);
+
+    expect(screen.getByText('Quantity: 0')).toBeInTheDocument();
+  });
+
+  it('renders correct quantity when numInCart is 3', () => {
+
+    renderWithProviders(<CartProductCard product={productInCart} />);
+
+    expect(screen.getByText('Quantity: 3')).toBeInTheDocument();
   });
 
 });
